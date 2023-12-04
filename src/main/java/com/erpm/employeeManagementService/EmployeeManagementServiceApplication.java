ffbcd7f9713759232bc2cbb9caf463e282208d0f
Omkar Parthe <partheomkar362@gmail.com>
2023-12-04 16:24:46 +0530

    eureka/discovery client configuration added

--- src/main/java/com/erpm/employeeManagementService/EmployeeManagementServiceApplication.java
+++ src/main/java/com/erpm/employeeManagementService/EmployeeManagementServiceApplication.java
@@ -2,8 +2,10 @@ package com.erpm.employeeManagementService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmployeeManagementServiceApplication {
 
 	public static void main(String[] args) {
